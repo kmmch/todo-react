7d01c62a4ac133285eb09971c061d483133a7604
kmmch <kmmch.tech@gmail.com>
2023-04-25 19:45:50 +0900

    CSS準備

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,36 @@ import React from 'react';
 import './styles.css';
 
 export const App = () => {
-    return <div></div>;
+    return (
+        <>
+            <div class="area input-area">
+                <input placeholder="TODOを入力"/>
+                <button>追加</button>
+            </div>
+            <div class="area imcomplete-area">
+                <p class="title">未完了のTODO</p>
+                <ul>
+                    <li class="row">
+                        <p>あああああ</p>
+                        <button>完了</button>
+                        <button>削除</button>
+                    </li>
+                    <li class="row">
+                        <p>いいいいい</p>
+                        <button>完了</button>
+                        <button>削除</button>
+                    </li>
+                </ul>
+            </div>
+            <div class="area complete-area">
+                <p class="title">完了のTODO</p>
+                <ul>
+                    <li class="row">
+                        <p>ううううう</p>
+                        <button>戻す</button>
+                    </li>
+                </ul>
+            </div>
+        </>
+    );
 }
