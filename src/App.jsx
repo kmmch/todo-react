5de6040257ebcf665482598f638ab7004f540b15
kmmch <kmmch.tech@gmail.com>
2023-04-25 19:53:04 +0900

    React実装を意識したモックに変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 
 export const App = () => {
+
+    // 未完了のTODO
+    const [incompleteTodos, setIncompleteTodos] = useState([
+        'あああああ','いいいいい'
+    ]);
+
+    // 完了のTODO
+    const [completeTodos, setCompleteTodos] = useState([
+        'ううううう'
+    ]);
+
     return (
         <>
             <div class="area input-area">
@@ -11,25 +22,28 @@ export const App = () => {
             <div class="area imcomplete-area">
                 <p class="title">未完了のTODO</p>
                 <ul>
-                    <li class="row">
-                        <p>あああああ</p>
-                        <button>完了</button>
-                        <button>削除</button>
-                    </li>
-                    <li class="row">
-                        <p>いいいいい</p>
-                        <button>完了</button>
-                        <button>削除</button>
-                    </li>
+                    {incompleteTodos.map((todo) => {
+                        return (
+                            <li class="row" key={ todo }>
+                                <p>{ todo }</p>
+                                <button>完了</button>
+                                <button>削除</button>
+                            </li>
+                        );
+                    })}
                 </ul>
             </div>
             <div class="area complete-area">
                 <p class="title">完了のTODO</p>
                 <ul>
-                    <li class="row">
-                        <p>ううううう</p>
-                        <button>戻す</button>
-                    </li>
+                    {completeTodos.map((todo) => {
+                        return (
+                            <li class="row" key={ todo }>
+                                <p>{ todo }</p>
+                                <button>戻す</button>
+                            </li>    
+                        );
+                    })}
                 </ul>
             </div>
         </>
